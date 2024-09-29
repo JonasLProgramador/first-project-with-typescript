0ff2a535d0ea38f215354e8b99234f7442e38a07
Jonas Leite <jonas.programador01@gmail.com>
2024-09-29 09:06:23 -0300

    feat: creating the model for negociation

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,5 @@
-alert("Hello world")
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao =  new Negociacao(new Date(), 3000, 400);
+console.log(negociacao);
+console.log(negociacao.valor);
