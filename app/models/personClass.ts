7cb47b60d26585271b12ea0799e2e3c51dae62ac
Jonas Leite <jonas.programador01@gmail.com>
2024-10-17 01:36:07 -0300

    :sparkles: implement  the new class for person

---
+++ app/models/personClass.ts
@@ -0,0 +1,13 @@
+class Person{
+    private readonly name ;
+    private readonly email;
+    private readonly password;
+    
+    constructor(name:string, email:string , password:string){
+        this.name= name;
+        this.email= email;
+        this.password = password
+    }
+   
+
+}
